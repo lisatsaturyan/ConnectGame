2266637469a7beee134ac245d6e9ee171ecf0354
Liza Tsaturyan <58171864+lisatsaturyan@users.noreply.github.com>
2024-05-24 05:28:36 -0700

    ms test
    test of the game, board and players

---
+++ GameTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConnectGame;
+
+namespace ConnectGameTest
+{
+
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void Game_InitializesCorrectly()
+        {
+            var game = new Game("Alice", "Bob");
+
+            Assert.IsNotNull(game.Player1);
+            Assert.IsNotNull(game.Player2);
+            Assert.IsNotNull(game.Board);
+
+            Assert.IsTrue(game.Player1.Token == 'X' || game.Player2.Token == 'X');
+            Assert.IsTrue(game.Player1.Token == 'O' || game.Player2.Token == 'O');
+        }
+
+        [TestMethod]
+        public void Game_SwitchesPlayersAfterTurn()
+        {
+            var game = new Game("Alice", "Bob");
+            var initialPlayer = game.CurrentPlayer;
+
+            game.PlayTurn(0);
+            Assert.AreNotEqual(initialPlayer, game.CurrentPlayer);
+        }
+    }
+}
+
